34450fad2374b483faef6946feb6a8e2e657c0e1
yobadagne <yobadagne@gmail.com>
2022-11-29 20:25:31 +0000

    ninth

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,3 +4,6 @@ def uppercase(str):
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
             print("{}".format(c), end="")
+        elif ord(c) == 8:
+            c = chr(8)
+            print ("{}".format(c))
