864a5dda73955ee2a096f66210994dd307ed45c6
yobadagne <yobadagne@gmail.com>
2022-12-06 09:41:56 +0000

    eigth

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = validate_tuple(tuple_a)
     tuple_b = validate_tuple(tuple_b)
-
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-
-
 def validate_tuple(_tuple=()):
     if len(_tuple) < 2:
         if len(_tuple) == 1:
