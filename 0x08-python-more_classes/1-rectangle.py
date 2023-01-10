2fbe0e61e2b9f304e66c7ef8919e174b445dee7d
yobadagne <yobadagne@gmail.com>
2023-01-10 10:21:43 +0000

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,12 @@ class Rectangle:
     """A rectangle class"""
 
     def __init__(self, width=0, height=0):
-        """Instantiate rectangle"""
+        """Instantiate rectangle.
+
+        Args:
+            width(int): The width of the rectangle
+            Height(int): The height of the rectangle
+        """
         if type(height) is not int:
             raise TypeError("height must be an integer ")
         elif height < 0:
