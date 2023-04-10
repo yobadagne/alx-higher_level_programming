e0a2a8d6afcb23dac57e74b4faaa600a56854151
yobadagne <yobadagne@gmail.com>
2023-04-10 10:28:59 +0000

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-# function that finds a peak in a list of unsorted integers.
-
-
-def aux(list_of_integers, first, last):
-    """ recursive auxiliary function """
-    x = last - first
-    if x == 1:
-        if list_of_integers[first] > list_of_integers[last]:
-            return first
-        else:
-            return last
-    middle = first + x // 2
-    if list_of_integers[middle] < list_of_integers[middle + 1]:
-        return aux(list_of_integers, middle, last)
-    return aux(list_of_integers, first, middle)
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """ function that finds a peak in a list of unsorted integers """
-    if len(list_of_integers) == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    return list_of_integers[aux(
-        list_of_integers, 0, len(list_of_integers) - 1)]
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
