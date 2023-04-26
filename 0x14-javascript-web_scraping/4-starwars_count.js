cbf5bc10648c458b69fb39bf92779ba44e9dba4c
yobadagne <yobadagne@gmail.com>
2023-04-26 11:21:37 +0000

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request.get(api, function (err, res, body) {
   const result = JSON.parse(body).results;
   let count = 0;
   for (let i = 0; i < JSON.parse(body).count; i++) {
-    const found = result[i].characters.find(element => element.includes('18'));
+    const found = result[i].characters.find(element => element.endsWith('/18/'));
     if (found) {
       count++;
     }
