9cd8b33d47d3e277aa8c30cbc67d95fafd4ccd69
yobadagne <yobadagne@gmail.com>
2023-04-26 11:40:09 +0000

    5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (error == null) {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
