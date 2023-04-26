e099280b50b28f40ed9826a54189a5ef383347fc
yobadagne <yobadagne@gmail.com>
2023-04-26 11:06:16 +0000

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,10 @@
 // looking for actor
 const request = require('request');
 const api = process.argv[2];
-request.get(api, function (res, body) {
+request.get(api, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
   const result = JSON.parse(body).results;
   let count = 0;
   for (let i = 0; i < JSON.parse(body).count; i++) {
