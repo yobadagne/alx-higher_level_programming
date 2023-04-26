4557ace09d37ad12a7080d036917d94be239a410
yobadagne <yobadagne@gmail.com>
2023-04-26 08:28:12 +0000

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@
 const request = require('request');
 const api = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request.get(api, function (err, res, body){
-console.log(JSON.parse(body).title);
+	console.log(JSON.parse(body).title);
 });
